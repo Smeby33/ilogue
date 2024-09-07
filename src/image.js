297aabfdc40f7a16a20dee3a765282ab8ff2aaa9
Smeby33 <smebedoh33@gmail.com>
2024-09-07 20:07:33 +0100

    lancer5

--- src/image.js
+++ src/image.js
@@ -1,11 +1,11 @@
 import Card from 'react-bootstrap/Card';
-import myImage from './assets/images/monImage.jpg';
+import monImage from './assets/images/monImage.png';
 
 function imgeA() {
   return (
     <>
       <Card>
-        <Card.Img variant="top" src={myImage} alt="Mon image" />
+        <Card.Img variant="top" src={monImage} alt="Mon image" />
       </Card>
     </>
   );
